60d4ec09d3bbbdd8498bb7d051b40e108de4eaf5
nks21may <nicolasdalessandro2@gmail.com>
2018-06-16 22:39:41 +0000

    -

---
+++ AdversaryFramework/Character.java
@@ -0,0 +1,14 @@
+public interface Character{
+	
+	abstract int getVida();
+
+	abstract int setVida();
+	
+	abstract int getCosto();
+	
+	abstract int setCosto();
+	
+	abstract int getDano();
+	
+	abstract int setDano();
+}
