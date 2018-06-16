e2f55f79d49e0878e7b75e087254790742351923
lsaenz94 <luchocba66@gmail.com>
2018-06-16 20:25:20 -0300

    automatic

---
+++ AdversaryFramework/Nuez.java
@@ -0,0 +1,29 @@
+package plantasVszombie;
+
+public class Nuez extends Plantas{
+	
+	public int getVida(){
+		return 0;
+	}
+	
+	public void setVida(int e){
+		
+	}
+	
+	public int getCosto(){
+		return 0;
+	}
+	
+	public void setCosto(int e){
+		
+	}
+	
+	public int getDano(){
+		return 0;
+	}
+	
+	public void setDano(int e){
+		
+	}
+	
+}
