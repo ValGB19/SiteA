ef5730f7b8043fea32e394ff11d8fcaab40abc8e
nks21may <nicolasdalessandro2@gmail.com>
2018-06-18 21:38:53 +0000

    asd

--- AdversaryFramework/Mein.java
+++
@@ -1,8 +0,0 @@
-public class Mein{
-	
-	public static void main(String[] args) {
-		ZombieLento l = new ZombieLento(200);
-		
-		System.out.println(l.getDano());
-	}
-}
--- AdversaryFramework/Zombie.java
+++
@@ -1,6 +0,0 @@
-public interface Zombie extends Character{
-	
-	public int getVel();
-
-	public void reciveDano(int d);
-}
