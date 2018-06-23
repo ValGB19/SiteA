d702e83a8e61650e8c55442a95f5e7884ce094cb
Nicolas Dalessandro <nicolasdalessandro2@gmail.com>
2018-06-23 20:01:58 -0300

    .

--- javas/Implements/Juego.java
+++ javas/Implements/Juego.java
@@ -1,49 +1,35 @@
 package javas.Implements;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javas.Interfaces.AdversaryFramework.AdversarySearchEngine;
 
 public class Juego extends AdversarySearchEngine<Problema,JardinExt>{
 	
-int cota=0;//usa en el metodo comp sucesor
-	
-	public Juego(Problema problem, int maxDepth) {
-		super(problem, maxDepth);
+	public int computeValue(JardinExt state) {	
+		return minMaxAB(state, maxDepth, problem.minValue(), problem.maxValue());
 	}
 
 
-	public int computeValue(JardinExt state) {
-		if (state.isMax() || maxDepth == 0) {
-			return problem.value(state);
-		}
-		Integer res = (state.getTurno()) ? problem.maxValue() : problem.minValue();
-		Juego aux = new Juego(problem, maxDepth-1);
-		for (JardinExt j :problem.getSuccessors(state)) {
-			res = (state.getTurno()) ? Math.min(aux.computeValue(j), res) : Math.max(aux.computeValue(j), res);
-		}
-		
-		return 0;
-	}
-
-
-	public JardinExt computeSuccessor(JardinExt state) {//El método ruleApplied () en el resultado indica qué regla condujo al estado. 
-			List<JardinExt> nuevo = new ArrayList<JardinExt>();
-			nuevo = problem.getSuccessors(state);
-			JardinExt mejor = null, masMejor = null;
-			if(cota <= getMaxDepth()){
-				for (JardinExt item : nuevo){
-					if((problem.value(state) <= problem.value(item))&& (state.getTurno() == true))
-						mejor = item;
-					
-					if((problem.value(state) > problem.value(item))&&(state.getTurno() == false))
-						mejor = state;
+	public JardinExt computeSuccessor(JardinExt state) {//El método ruleApplied () en el resultado indica qué regla condujo al estado.
+		JardinExt mejor = null;
+		int im = (state.getTurno()) ? problem.minValue(): problem.maxValue();
+		int r;
+		for (JardinExt item : problem.getSuccessors(state)){
+			r = computeValue(item);
+			if(state.getTurno()) {
+				if (r > im) {
+					mejor = item;
+					im = r;
 				}
-				cota++;
-				masMejor = computeSuccessor(mejor);
+			}else {
+				if (r < im) {
+					mejor = item;
+					im = r;
+				}
 			}
-			return masMejor;
+		}
+		return mejor;
 	}
 	
 	public int minMaxAB(JardinExt raiz, int profundidad, int alfa, int beta){
@@ -66,15 +52,8 @@ int cota=0;//usa en el metodo comp sucesor
 		}
 	}
 	
-	/*Funcion minMaxAB(n, alfa, beta) --> Valor!  
-	 *   Si n es hoja o esta a nivel maximo!    
-	 *       retornar valor(n)!
-	 *           Sino!
-	 *         Para cada hijo n_k de n y mientras alfa<beta hacer! 
-	 *                    Si n es Max!                alfa:= max(alfa, minMaxAB(n_k, alfa, beta))!            Sino!                beta:= min(beta, minMaxAB(n_k, alfa, beta))!            Fsi  !        Fpara!        Si n es Max!            retornar alfa!        Sino!            retornar beta!        Fsi!    Fsi ! Ffuncion
-*/
-
 	public void report() {
-		
+		System.out.println("Max depth: "+maxDepth);
+		System.out.println("isMax? "+problem.initialState().isMax());
 	}
 }
